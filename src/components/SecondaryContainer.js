a99c67366a4896d984d033808e5a6d0a99500f9c
Rohit <2020010006264@gndu.ac.in>
2024-08-18 15:31:43 +0530

    testing github

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const SecondaryContainer = () => {
-  return <div className="bg-pink-500">SecondaryContainer</div>;
+  return (
+    <div className="bg-pink-500">
+      SecondaryContainer
+      <h1>Testing</h1>
+    </div>
+  );
 };
 
 export default SecondaryContainer;
